a5b39e8eb19b4e0f51858b72cdc2ba9ae4ac8f23
OST <ost5253@gmail.com>
2016-12-08 10:54:02 +0900

    mailbox ING repair

--- database/migrations/2016_12_08_104553_alter_mailboxes_table.php
+++ database/migrations/2016_12_08_104553_alter_mailboxes_table.php
@@ -13,21 +13,21 @@ class AlterMailboxesTable extends Migration
      */
     public function up()
     {
-        Schema::table('mailboxes', function ($table) {
-            $table->dropColumn('to');
-            $table->dropColumn('from');
-        });
-
-        Schema::table('mailboxes', function ($table) {
-
-            $table->integer('to')->unsigned()->index();
-            $table->integer('from')->unsigned()->index();
-            $table->integer('novel_id')->unsigned()->index();
-
-            $table->foreign('to')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('from')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('novel_id')->references('id')->on('novels')->onDelete('cascade');
-        });
+//        Schema::table('mailboxes', function ($table) {
+//            $table->dropColumn('to');
+//            $table->dropColumn('from');
+//        });
+//
+//        Schema::table('mailboxes', function ($table) {
+//
+//            $table->integer('to')->unsigned()->index();
+//            $table->integer('from')->unsigned()->index();
+//            $table->integer('novel_id')->unsigned()->index();
+//
+//            $table->foreign('to')->references('id')->on('users')->onDelete('cascade');
+//            $table->foreign('from')->references('id')->on('users')->onDelete('cascade');
+//            $table->foreign('novel_id')->references('id')->on('novels')->onDelete('cascade');
+//        });
     }
 
     /**
@@ -37,9 +37,9 @@ class AlterMailboxesTable extends Migration
      */
     public function down()
     {
-        Schema::table('mailboxes', function ($table) {
-            $table->dropColumn('novel_id');
-
-        });
+//        Schema::table('mailboxes', function ($table) {
+//            $table->dropColumn('novel_id');
+//
+//        });
     }
 }
