ea03028c52422a4de6f9fba97913a498d644a8c7
OST <ost5253@gmail.com>
2016-12-13 10:47:31 +0900

    admin index page

--- app/Http/Controllers/PageController/AdminPageController.php
+++ app/Http/Controllers/PageController/AdminPageController.php
@@ -40,54 +40,15 @@ class AdminPageController extends Controller
         $reser_day = new Carbon();
 
         //�Ⱓ������ ���ٸ� null ���� �����Ѵ�
-        if($novel->publish_reservation == null){
+        if ($novel->publish_reservation == null) {
             $novel->reser_day = null;
             $novel->reser_time = null;
-        }
-        else{
+        } else {
             $novel->reser_day = $reser_day->toDateString();
             $novel->reser_day = $reser_day->format('h:i');
         }
 
         return view('admin.novel_inning_view', compact('novel', 'novel_group'));
-    }
-
-
-    public function novel_json(Request $request)
-    {
-        //
-        $novel_groups = NovelGroup::all("*");
-
-        $comments_count = 0;
-        $review_count = 0;
-        $count_data = array();
-        $review_count_data = array();
-        $latested_at = array();
-
-        foreach ($novel_groups as $novel_group) {
-
-            foreach ($novel_group->novels as $novel) {
-                foreach ($novel->comments as $commenat) {
-                    $comments_count++;
-                }
-                foreach ($novel->reviews as $n) {
-                    $review_count++;
-                }
-
-            }
-            $latested_at[$novel_group->id] = $novel_group->novels->sortby('created_at')->first()->created_at->format('Y-m-d');
-
-            $count_data[$novel_group->id] = $comments_count;
-            $comments_count = 0;
-
-            $review_count_data[$novel_group->id] = $review_count;
-            $review_count = 0;
-
-        }
-        // dd($count_data);
-        // $novel_group= $request->user()->novel_groups()->where('id',$user_novels->novel_group_id)->first();
-        return \Response::json(['novel_groups' => $novel_groups, 'count_data' => $count_data, 'review_count_data' => $review_count_data, 'latested_at' => $latested_at]);
-        // dd($user_novels);
     }
 
     public function user()
@@ -118,7 +79,7 @@ class AdminPageController extends Controller
 
     public function memo(Request $request)
     {
-        $novel_mail_messages= Mailbox::all('*');
+        $novel_mail_messages = Mailbox::all('*');
         return view('admin.memo', compact('novel_mail_messages'));
     }
 
