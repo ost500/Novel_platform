f8141767b2c52b28d8ea7c61bfb8d61c4ddca2f1
OST <ost5253@gmail.com>
2016-12-28 14:47:14 +0900

    publishing layout

--- resources/views/author/partnership/apply_list.blade.php
+++ resources/views/author/partnership/apply_list.blade.php
@@ -26,38 +26,50 @@
                                 <table class="table table-striped table-hover">
                                     <thead>
                                     <tr>
-                                        <th class="">작품명</th>
-                                        <th class="">업체명</th>
-                                        <th class="text-center">초기연재회차</th>
-                                        <th class="text-center">일연재회차</th>
-                                        <th class="text-center">신청일</th>
-                                        <th class="text-center">처리일</th>
-                                        <th class="text-center">상태</th>
+                                        <th>작품명</th>
+                                        <th class="text-center">일</th>
+                                        <th class="text-center">편수</th>
+                                        <th class="text-center">네이버</th>
+                                        <th class="text-center">카카오</th>
+                                        <th class="text-center">예스24</th>
+                                        <th class="text-center">교보</th>
                                     </tr>
                                     </thead>
                                     <tbody>
 
                                     <tr>
                                         <td class="col-md-2">작품명1</td>
-                                        <td class="col-md-3">업체명1</td>
-                                        <td class="col-md-1 text-center">20회차</td>
+                                        <td class="col-md-1 text-center">1일</td>
                                         <td class="col-md-1 text-center">2회차</td>
-                                        <td class="col-md-2 text-center">2016-12-20 11:00:12</td>
-                                        <td class="col-md-2 text-center">2016-12-20 11:00:12</td>
-                                        <td class="text-center">
-                                            <button class="btn btn-warning">승인대기</button>
+                                        <td class="col-md-2 text-center">
+                                            <button class="btn btn-primary">신청하기</button>
+                                        </td>
+                                        <td class="col-md-2 text-center">
+                                            <button class="btn btn-warning">대기 중</button>
+                                        </td>
+                                        <td class="col-md-2 text-center">
+                                            <button class="btn btn-danger">승인</button>
+                                        </td>
+                                        <td class="col-md-2 text-center">
+                                            <button class="btn btn-purple">심사 중</button>
                                         </td>
                                     </tr>
 
                                     <tr>
                                         <td class="col-md-2">작품명1</td>
-                                        <td class="col-md-3">업체명1</td>
-                                        <td class="col-md-1 text-center">20회차</td>
+                                        <td class="col-md-1 text-center">1일</td>
                                         <td class="col-md-1 text-center">2회차</td>
-                                        <td class="col-md-2 text-center">2016-12-20 11:00:12</td>
-                                        <td class="col-md-2 text-center">2016-12-20 11:00:12</td>
-                                        <td class="text-center">
-                                            <button class="btn btn-success">승인완료</button>
+                                        <td class="col-md-2 text-center">
+                                            <button class="btn btn-primary">신청하기</button>
+                                        </td>
+                                        <td class="col-md-2 text-center">
+                                            <button class="btn btn-warning">대기 중</button>
+                                        </td>
+                                        <td class="col-md-2 text-center">
+                                            <button class="btn btn-danger">승인</button>
+                                        </td>
+                                        <td class="col-md-2 text-center">
+                                            <button class="btn btn-purple">심사 중</button>
                                         </td>
                                     </tr>
                                     </tbody>
