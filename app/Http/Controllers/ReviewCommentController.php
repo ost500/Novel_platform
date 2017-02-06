264b20e5aa0c0af8b781d4432e8e4fc682adda7b
Devender <devender0709@gmail.com>
2017-02-06 15:39:34 +0900

    block mail , block free board and review posting

--- app/Http/Controllers/ReviewCommentController.php
+++ app/Http/Controllers/ReviewCommentController.php
@@ -23,6 +23,12 @@ class ReviewCommentController extends Controller
             'comment.max' => '글이 너무 깁니다',
         ])->validate();
 
+        //if posting is blocked then redirect back
+        if (Auth::user()->isPostingBlocked()) {
+            $errors= '독자추천 댓글 기능이 관리자에 의해 금지 됐습니다';
+            return redirect()->back()->withErrors($errors);
+        }
+
         $new_comment = new ReviewComment();
         $new_comment->user_id = Auth::user()->id;
         $new_comment->review_id = $id;
