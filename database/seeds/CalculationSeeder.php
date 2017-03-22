4350e0f3f4f9d27cc86e136ab403496cd281c7f0
OST <ost5253@gmail.com>
2017-03-22 15:37:22 +0900

    seeder edited

--- database/seeds/CalculationSeeder.php
+++ database/seeds/CalculationSeeder.php
@@ -21,6 +21,8 @@ class CalculationSeeder extends Seeder
         $cal->column_names = "name,컨텐츠No, 공급자코드, CP명, 서비스일";
         $cal->excel_file = '/excel/' . 'naver.xlsx';
         $cal->description = "this is what i want";
+        $cal->when = "2017-03-22";
+        $cal->cal_numberX = "C";
         $cal->save();
     }
 }
