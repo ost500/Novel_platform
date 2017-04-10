b929e5a2e44ba5f88d4133f9ad2fc2bb89457afa
Devender <devender0709@gmail.com>
2017-04-10 10:35:47 +0900

    NOV-248  Edit page link on novel title

--- resources/views/author/novel_group.blade.php
+++ resources/views/author/novel_group.blade.php
@@ -36,8 +36,8 @@
 
                                     <tr v-for="novel in novels">
                                         <td class="text-center col-md-1">@{{ novel.inning }}화</td>
-                                        <td class="col-md-8"><a href="# "
-                                                                v-on:click="go_to_novel(novel.id)">@{{ novel.title }}</a>
+                                        <td class="col-md-8"><a href="#"
+                                                                v-on:click="go_to_update(novel.id)">@{{ novel.title }}</a>
                                             <button v-if="novel.adult != 0" class="btn btn-xs btn-danger btn-circle">
                                                 19금
                                             </button>
