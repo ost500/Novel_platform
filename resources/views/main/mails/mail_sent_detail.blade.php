b9994c93832bdff3fcbec1cf91d1dba33ddb4f2f
OST <ost5253@gmail.com>
2017-04-10 09:47:25 +0900

    작성일 -> 발송시각

--- resources/views/main/mails/mail_sent_detail.blade.php
+++ resources/views/main/mails/mail_sent_detail.blade.php
@@ -42,7 +42,7 @@
                     <h2 class="bbs-view-title">{{ $mail->subject }}</h2>
                     <div class="bbs-view-info">
                         <div class="writer">{{ $mail->users->nickname}}</div>
-                        <div class="etc"><span>작성일 {{ $mail->created_at }}</span>
+                        <div class="etc"><span>발송시각 {{ $mail->created_at }}</span>
                             {{--<span>조회수 {{ $mail->view_count }}</span></div>--}}
                         </div>
                     {{--<div class="bbs-view-manage"><a href="#mode_nav"><i class="setup-icon">수정</i></a></div>--}}
